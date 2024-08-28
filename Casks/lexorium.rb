c0f57856256c9503cb9f167aca1f975f018061b7
Marius Antonsen <antonsen.marius@gmail.com>
2024-08-28 23:16:32 +0200

    Update lexorium.rb

--- Casks/lexorium.rb
+++ Casks/lexorium.rb
@@ -1,8 +1,8 @@
 cask "lexorium" do
-  version "1.0.9"
-  sha256 "e445522532a05f4eaf233da379c301352858acb42250e4276ab4326fdf4f1f2e"
+  version "1.1.0"
+  sha256 "bb006f347284cde2fc0aff13db1cef8473c38dc128019d1b98c00559d3a6063b"
 
-  url "https://github.com/antnsn/Lexorium/releases/download/v#{version}/Lexorium-darwin-x64-#{version}.zip"
+  url "https://github.com/antnsn/Lexorium/releases/download/v#{version}/Lexorium-darwin-arm64-#{version}.zip"
   name "Lexorium"
   desc "Manage notes with markdown support"
   homepage "https://github.com/antnsn/Lexorium"
@@ -15,13 +15,6 @@ cask "lexorium" do
   ]
 
   caveats <<~EOS
-    This app requires Rosetta 2 to run on Apple Silicon (M1/M2) Macs.
-    If Rosetta 2 is not already installed, you can install it by running:
-
-      softwareupdate --install-rosetta
-
-    After Rosetta 2 is installed, you can run the app normally.
-
     Note: Due to macOS security settings, you may need to right-click the app and choose 'Open' to allow it to run the first time.
   EOS
 end
