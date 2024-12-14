1b9011c27eb665367f8b9a30de4b8ba1ba559516
antnsn <antonsen.marius@gmail.com>
2024-12-14 15:08:41 +0000

    Update Homebrew formula to version v3.0.2

--- Casks/lexorium.rb
+++ Casks/lexorium.rb
@@ -1,6 +1,6 @@
 cask "lexorium" do
   version "3.0.2"
-  sha256 "ca89d694463237639bfbb35ecd351af3ce7158ce81fbc25db456cd36696647c2"
+  sha256 "a6f2eb868aa59cfb63c31a770aaff39d1c994a68630cc5a80cee0cbf075d037a"
 
   url "https://github.com/antnsn/Lexorium/releases/download/v#{version}/Lexorium-darwin-arm64-#{version}.zip"
   name "Lexorium"
